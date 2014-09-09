dc05678eb55b1b4150ee9e327f3f6de19b7f95d6
tahmilur <tahmilur@yahoo.com>
2014-09-09 13:17:20 +0200

    Add Abstract class

---
+++ ClassExample/AbstractClassExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassExample
+{
+    abstract class Motorcycle
+    {
+        // Anyone can call this. 
+        public void StartEngine() {/* Method statements here */ }
+
+        // Only derived classes can call this. 
+        protected void AddGas(int gallons) { /* Method statements here */ }
+
+        // Derived classes can override the base class implementation. 
+        public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
+
+        // Derived classes must implement this. 
+        public abstract double GetTopSpeed();
+    }
+
+    class Abarth : Motorcycle
+    {
+        public override double GetTopSpeed()
+        {
+            return 108.4;
+        }
+    }
+
+    class BMW : Motorcycle
+    {
+        public override double GetTopSpeed()
+        {
+            return 250;
+        }
+    }
+
+    class Ferrari : Motorcycle
+    {
+        public override double GetTopSpeed()
+        {
+            return 250;
+        }
+    }
+
+    class Mercedes : Motorcycle
+    {
+        public override double GetTopSpeed()
+        {
+            return 150;
+        }
+    }
+}

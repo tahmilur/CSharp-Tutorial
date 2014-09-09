05d91b0520e9b641c3579dfc1f192d6b85488392
tahmilur <tahmilur@yahoo.com>
2014-09-09 12:39:23 +0200

    Hello world example

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Main program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main entry point of a C# program. This program displays the string 
+        /// "Hello World!" on the screen.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Write a line in the console in debug mode
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+
+            // Wait for user input in console
+            Console.ReadKey();
+        }
+    }
+}

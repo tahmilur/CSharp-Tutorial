052145debb3e19ed5b5e6bb9208ac13be6215a07
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:20:58 +0200

    Extension example

---
+++ ExtensionExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Console.WriteLine(x.Multiply(20));
+
+            Console.Read();
+        }
+    }
+
+    public static class IntExtension
+    {
+        public static int Multiply(this int x, int y)
+        {
+            return x * y;
+        }
+    }
+}

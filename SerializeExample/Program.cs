828ddc3db91159e05fd96ac1dceda2ed61bd6d9c
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:31:55 +0200

    Serialize example

---
+++ SerializeExample/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            string xml;
+
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Person p = new Person
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Age = 42
+                };
+                serializer.Serialize(stringWriter, p);
+                xml = stringWriter.ToString();
+            }
+
+            Console.WriteLine(xml);
+
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                Person p = (Person)serializer.Deserialize(stringReader);
+                Console.WriteLine("{0} {1} is {2} years old", p.FirstName, p.LastName, p.Age);
+            }
+
+            //
+            serializer = new XmlSerializer(typeof(Order), new Type[] { typeof(VIPOrder) });
+
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Order order = CreateOrder();
+                serializer.Serialize(stringWriter, order);
+                xml = stringWriter.ToString();
+            }
+
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                Order o = (Order)serializer.Deserialize(stringReader);
+                // Use the order
+            }
+
+            Console.Read();
+        }
+
+        private static Order CreateOrder()
+        {
+            Product p1 = new Product { ID = 1, Description = "p2", Price = 9 };
+            Product p2 = new Product { ID = 2, Description = "p3", Price = 6 };
+
+            Order order = new VIPOrder
+            {
+                ID = 4,
+                Description = "Order for John Doe. Use the nice giftwrap",
+                OrderLines = new List<OrderLine>
+                { 
+                    new OrderLine { ID = 5, Amount = 1, Product = p1},
+                    new OrderLine { ID = 6 ,Amount = 10, Product = p2},
+                }
+            };
+
+            return order;
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        [NonSerialized]
+        private bool isDirty = false;
+
+        [OnSerializing()]
+        internal void OnSerializingMethod(StreamingContext context)
+        {
+            Console.WriteLine("OnSerializing.");
+        }
+
+        [OnSerialized()]
+        internal void OnSerializedMethod(StreamingContext context)
+        {
+            Console.WriteLine("OnSerialized.");
+        }
+
+        [OnDeserializing()]
+        internal void OnDeserializingMethod(StreamingContext context)
+        {
+            Console.WriteLine("OnDeserializing.");
+        }
+
+        [OnDeserialized()]
+        internal void OnDeserializedMethod(StreamingContext context)
+        {
+            Console.WriteLine("OnSerialized.");
+        }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+    }
+
+    [Serializable]
+    public class PersonComplex : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private bool isDirty = false;
+
+        public PersonComplex() { }
+
+        protected PersonComplex(SerializationInfo info, StreamingContext context)
+        {
+            Id = info.GetInt32("Value1");
+            Name = info.GetString("Value2");
+            isDirty = info.GetBoolean("Value3");
+        }
+
+        [System.Security.Permissions.SecurityPermission(SecurityAction.Demand,
+                                                        SerializationFormatter = true)]
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Value1", Id);
+            info.AddValue("Value2", Name);
+            info.AddValue("Value3", isDirty);
+        }
+    }
+
+    [Serializable]
+    public class Order
+    {
+        [XmlAttribute]
+        public int ID { get; set; }
+
+        [XmlIgnore]
+        public bool IsDirty { get; set; }
+        [XmlArray("Lines")]
+        [XmlArrayItem("OrderLine")]
+        public List<OrderLine> OrderLines { get; set; }
+    }
+
+    [Serializable]
+    public class VIPOrder : Order
+    {
+        public string Description { get; set; }
+    }
+
+    [Serializable]
+    public class OrderLine
+    {
+        [XmlAttribute]
+        public int ID { get; set; }
+
+        [XmlAttribute]
+        public int Amount { get; set; }
+
+        [XmlElement("OrderedProduct")]
+        public Product Product { get; set; }
+    }
+
+    [Serializable]
+    public class Product
+    {
+        [XmlAttribute]
+        public int ID { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}

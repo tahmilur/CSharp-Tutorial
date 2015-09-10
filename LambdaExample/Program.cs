1054361fed0dbec06fdee630375273392edb0789
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:19:14 +0200

    Lamda Example

---
+++ LambdaExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> myDelegate = delegate(int i) { return i * 2; };
+            Func<int, int> myDelegate1 = x => x * 2;
+
+            Func<int, int, int> mySum = (int x, int y) => { return (x + y); };
+            Func<int> mytest = () => 20;
+
+            Console.WriteLine(myDelegate(21)); // Displays 42
+            Console.WriteLine(myDelegate1(10)); // Displays 4
+            Console.WriteLine(mySum(10, 100)); // Displays 4
+
+            Console.Read();
+        }
+    }
+}

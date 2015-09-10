a83b9a747950f35ae05171d6e0d10e0deb6c27c8
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:20:33 +0200

    File Example

---
+++ FileExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            // Path example
+            
+            foreach (var item in Path.GetInvalidFileNameChars())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in Path.GetInvalidPathChars())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("RandomFileName: " + Path.GetRandomFileName());
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+
+            Console.Read();
+        }
+
+        private static void CopyFile()
+        {
+            string path = @"c:\temp\test.txt";
+            string destPath = @"c:\temp\destTest.txt";
+
+            File.CreateText(path).Close();
+            File.Copy(path, destPath);
+
+            FileInfo fileInfo = new FileInfo(path);
+            fileInfo.CopyTo(destPath);
+        }
+
+        private static void MoveFile()
+        {
+            // Moving a file
+            string path = @"c:\temp\test.txt";
+            string destPath = @"c:\temp\destTest.txt";
+
+            File.CreateText(path).Close();
+            File.Move(path, destPath);
+
+            FileInfo fileInfo = new FileInfo(path);
+            fileInfo.MoveTo(destPath);
+        }
+    }
+}

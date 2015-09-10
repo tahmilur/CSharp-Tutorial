825496209ad6e91140f5f8de41d2317360ade5f4
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:29:26 +0200

    secure string example

---
+++ SecureStringExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureStringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SecureString ss = new SecureString())
+            {
+                Console.Write("Please enter password: ");
+
+                while (true)
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.Enter) break;
+
+                    ss.AppendChar(cki.KeyChar);
+                    Console.Write("*");
+                }
+                ss.MakeReadOnly();
+
+                Console.WriteLine();
+                ConvertToUnsecureString(ss);
+            }           
+
+            Console.ReadLine();
+        }
+
+        // Getting the value of a SecureString
+        public static void ConvertToUnsecureString(SecureString securePassword)
+        {
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+
+                Console.WriteLine(Marshal.PtrToStringUni(unmanagedString));
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+    }
+}

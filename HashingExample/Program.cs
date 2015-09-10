c3e320107e957477346d6ba1e4f8d7f2dce9c389
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:21:27 +0200

    Hashing Example

---
+++ HashingExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            SHA256 sha256 = SHA256.Create();
+
+            string data = "A paragraph of text";
+            byte[] hashA = sha256.ComputeHash(byteConverter.GetBytes(data));
+
+            data = "A paragraph of changed text";
+            byte[] hashB = sha256.ComputeHash(byteConverter.GetBytes(data));
+
+            data = "A paragraph of text";
+            byte[] hashC = sha256.ComputeHash(byteConverter.GetBytes(data));
+
+            Console.WriteLine(hashA.SequenceEqual(hashB)); // Displays: false
+            Console.WriteLine(hashA.SequenceEqual(hashC)); // Displays: true
+
+            Console.Read();
+        }
+    }
+}

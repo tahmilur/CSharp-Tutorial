56162d0d11ef8183e66a5b8d91d9e6cdcb026130
tahmilur <tahmilur@yahoo.com>
2015-09-10 13:23:52 +0200

    JSON Example

---
+++ JSONExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JSONExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "{\"employees\":[{\"firstName\":\"John\", \"lastName\":\"Doe\"}, {\"firstName\":\"Anna\", \"lastName\":\"Smith\"}, {\"firstName\":\"Peter\",\"lastName\":\"Jones\"}]}";
+            var serializer = new JavaScriptSerializer();
+            var result = serializer.Deserialize<Dictionary<string, object>>(json);
+
+            Console.Read();
+        }
+    }
+}
